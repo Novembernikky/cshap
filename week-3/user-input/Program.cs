99b2d6b56fe4bcc6853d042af660bbb09fcd485e
Novembernikky <142704902+Novembernikky@users.noreply.github.com>
2023-08-27 17:36:16 +0700

    Add files via upload

---
+++ week-3/user-input/Program.cs
@@ -0,0 +1,21 @@
+﻿string fname = "";
+double height = 0;
+double weight = 0.0d;
+double BMI = 0.0d;
+
+
+Console.Write("input first name : "); //display
+fname = Console.ReadLine();        //input keyboard
+
+Console.Write("input height : "); //display
+height = double.Parse(Console.ReadLine());     //input keyboard
+
+Console.Write("input weight : "); //display
+weight = double.Parse(Console.ReadLine());    //input keyboard
+
+BMI = height/weight;
+
+BMI = Math.Pow(BMI, 2);
+
+
+Console.WriteLine("BMI is : "+BMI); //หาค่าดัชนี มวลกาย
