886f1d33a3019d033ddfe5b9cb860fe2f09936f9
Novembernikky <142704902+Novembernikky@users.noreply.github.com>
2023-08-27 20:41:49 +0700

    Add files via upload

--- week-3/user-input/Program.cs
+++ week-3/user-input/Program.cs
@@ -1,4 +1,5 @@
-﻿string fname = "";
+﻿/*
+string fname = "";
 double height = 0;
 double weight = 0.0d;
 double BMI = 0.0d;
@@ -19,3 +20,54 @@ BMI = Math.Pow(BMI, 2);
 
 
 Console.WriteLine("BMI is : "+BMI); //หาค่าดัชนี มวลกาย
+*/
+
+
+string prefix = "";
+string fname = "";
+string lname = "";
+int id = 0;
+double height = 0d;
+double weight = 0.0d;
+double BMI = 0.0d;
+
+
+Console.Write("prefix : "); //display
+prefix = Console.ReadLine();        //input keyboard
+
+Console.Write(" first name : "); //display
+fname = Console.ReadLine();        //input keyboard
+
+Console.Write("last name : "); //display
+lname = Console.ReadLine();     //input keyboard
+
+Console.Write("student id : "); //display
+id = Int32.Parse(Console.ReadLine());     //input keyboard
+
+Console.Write("input height : "); //display
+height = double.Parse(Console.ReadLine());     //input keyboard
+
+Console.Write("input weight : "); //display
+weight = double.Parse(Console.ReadLine());    //input keyboard
+
+BMI = height/weight;
+
+BMI = Math.Pow(BMI, 2);
+
+
+Console.WriteLine("******************** "); //display
+Console.WriteLine("Student ID is "+id ); //display
+Console.WriteLine("fullname : "+prefix+" "+fname+" "+lname ); //display
+Console.WriteLine("your height :"+height); //display
+Console.WriteLine("your weight :"+weight); //display
+Console.WriteLine("BMI is : "+BMI); //display
+Console.WriteLine("******************** "); //display
+
+
+
+
+
+
+
+
+
